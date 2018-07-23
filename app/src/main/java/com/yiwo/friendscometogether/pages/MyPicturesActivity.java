7704a583889ca07394dcd4729128f62d8ea8f760
huweidongls <466463054@qq.com>
2018-07-23 15:52:10 +0800

    1

--- app/src/main/java/com/yiwo/friendscometogether/pages/MyPicturesActivity.java
+++ app/src/main/java/com/yiwo/friendscometogether/pages/MyPicturesActivity.java
@@ -2,8 +2,11 @@ package com.yiwo.friendscometogether.pages;
 
 import android.os.Bundle;
 
+import com.yatoooon.screenadaptation.ScreenAdapterTools;
 import com.yiwo.friendscometogether.R;
 import com.yiwo.friendscometogether.base.BaseActivity;
+
+import butterknife.ButterKnife;
 
 public class MyPicturesActivity extends BaseActivity {
 
@@ -11,5 +14,23 @@ public class MyPicturesActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_pictures);
+        ScreenAdapterTools.getInstance().loadView(getWindow().getDecorView());
+
+        ButterKnife.bind(this);
+
+        initData();
+
+    }
+
+    private void initData() {
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        MyPicturesActivity.this.finish();
     }
 }
