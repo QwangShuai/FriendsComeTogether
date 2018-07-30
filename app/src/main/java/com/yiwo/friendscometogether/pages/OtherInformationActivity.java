f84267f686724dc4881be63dfebe9a7809064f9f
huweidongls <466463054@qq.com>
2018-07-30 11:23:55 +0800

    1

--- app/src/main/java/com/yiwo/friendscometogether/pages/OtherInformationActivity.java
+++ app/src/main/java/com/yiwo/friendscometogether/pages/OtherInformationActivity.java
@@ -2,8 +2,11 @@ package com.yiwo.friendscometogether.pages;
 
 import android.os.Bundle;
 
+import com.yatoooon.screenadaptation.ScreenAdapterTools;
 import com.yiwo.friendscometogether.R;
 import com.yiwo.friendscometogether.base.BaseActivity;
+
+import butterknife.ButterKnife;
 
 public class OtherInformationActivity extends BaseActivity {
 
@@ -11,5 +14,17 @@ public class OtherInformationActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_information);
+        ScreenAdapterTools.getInstance().loadView(getWindow().getDecorView());
+
+        ButterKnife.bind(this);
+
+        initData();
+
+    }
+
+    private void initData() {
+
+
+
     }
 }
